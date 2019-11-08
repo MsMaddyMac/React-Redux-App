4561d5aaeae24ae8af7e91bde114b626ac3aba7a
Maddy <mmcintosh2k14@gmail.com>
2019-11-07 19:55:50 -0500

    adds cleaned up code

--- src/components/ElephantList.js
+++ src/components/ElephantList.js
@@ -7,9 +7,9 @@ import { ElephantCard } from './ElephantCard'
 
 function ElephantList(props) {
     return (
-        <>
+        <div className="elephant-list">
             <h4>Press button to see Elephants!</h4>
-            <button onClick={() => props.dispatch(fetchElephants())}>Enter</button>
+            <button className="btn" onClick={() => props.dispatch(fetchElephants())}>Enter</button>
             {props.isFetching && <span> Here comes the herd 🐘🐘🐘!!</span>}
             {props.error && <div>{props.error.message}</div>}
             <div className="allCards">
@@ -17,13 +17,10 @@ function ElephantList(props) {
                     <ElephantCard elephant={elephant} key={id} />
                 ))}
             </div>
-        </>
+        </div>
     );
 };
 
-const mapStateToProps = {
-    fetchElephants
-};
 
 export default connect(state => {
     return state;})(ElephantList);
